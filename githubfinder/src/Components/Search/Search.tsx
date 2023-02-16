480752ab3e31a9ab574654ec3eac48c1bc17e421
FelipeKreulich <felipek2002k@gmail.com>
2023-02-16 19:17:35 -0400

    Create a component Home and Search

---
+++ githubfinder/src/Components/Search/Search.tsx
@@ -0,0 +1,27 @@
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>
+};
+
+import { useState } from 'react'
+import { BsSearch } from 'react-icons/bs';
+import styles from './Search.module.css'
+
+const Search = ({loadUser}: SearchProps) => {
+
+  const [userName, setuserName] = useState("")
+
+  return (
+    <div className={styles.search}>
+      <h2>Busque por um Usuário:</h2>
+      <p>Conheça seus melhores repositórios!</p>
+      <div className={styles.search_container}>
+        <input type="text" placeholder='Digite o nome do Usuário' onChange={(e) => setuserName(e.target.value)}/>
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Search
